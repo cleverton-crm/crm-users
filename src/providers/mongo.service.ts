8cb7f60c55f2615682e0b9ce7d8f10f9c6952af2
Alexander Hunter <icehuntmen@gmail.com>
2021-12-12 12:21:56 +0300

    Replica setup

--- src/providers/mongo.service.ts
+++ src/providers/mongo.service.ts
@@ -2,19 +2,27 @@ import {
   MongooseModuleOptions,
   MongooseOptionsFactory,
 } from '@nestjs/mongoose';
-import { getBoolean } from '../helpers/global';
 
 export class MongoConfigService implements MongooseOptionsFactory {
   createMongooseOptions(): MongooseModuleOptions {
     let urlMongo;
+    const replica = process.env.MONGO_REPLICA;
     const auth = process.env.MONGO_ENABLE;
     const user = process.env.MONGO_ROOT_USER;
     const password = process.env.MONGO_ROOT_PASSWORD;
-    const host = process.env.MONGO_HOST;
+    const host1 = process.env.MONGO_HOST1;
+    const host2 = process.env.MONGO_HOST2;
+    const host3 = process.env.MONGO_HOST3;
     const base = process.env.MONGO_DATABASE;
-    const port = process.env.MONGO_PORT;
+    const port1 = process.env.MONGO_PORT1;
+    const port2 = process.env.MONGO_PORT2;
+    const port3 = process.env.MONGO_PORT3;
 
-    urlMongo = `mongodb://${user}:${password}@${host}:${port}/${base}?authSource=admin`;
+    if (replica) {
+      urlMongo = `mongodb://${user}:${password}@${host1}:${port1},${host2}:${port2},${host3}:${port3}/${base}?authSource=admin`;
+    } else {
+      urlMongo = `mongodb://${user}:${password}@${host1}:${port1}/${base}?authSource=admin`;
+    }
 
     return {
       uri: urlMongo,
