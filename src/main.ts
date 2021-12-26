4b771164f8abedfb420b4c2fd66d4d9f24bdf549
Alexander Hunter <icehuntmen@gmail.com>
2021-12-26 09:54:13 +0300

    Изменена ссылка на AppModule

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { UserModule } from './user.module';
+import { AppModule } from './app.module';
 import { TcpOptions, Transport } from '@nestjs/microservices';
 import { Logger } from '@nestjs/common';
 import { cyan } from 'cli-color';
@@ -9,7 +9,7 @@ require('dotenv').config();
 async function bootstrap() {
   const logger = new Logger('UserModule');
   const config = new ConfigService();
-  const app = await NestFactory.createMicroservice(UserModule, {
+  const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.TCP,
     options: {
       host: '0.0.0.0',
