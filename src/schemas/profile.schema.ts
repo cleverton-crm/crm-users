8967d317145da735fee37b7423b286336fb477f0
Alexander Hunter <icehuntmen@gmail.com>
2022-01-08 15:27:11 +0300

    Расширены поля для профиля пользователя (обязательные)

--- src/schemas/profile.schema.ts
+++ src/schemas/profile.schema.ts
@@ -12,6 +12,15 @@ export class Profile extends Document {
 
   @Prop({ type: String, unique: true })
   owner: string;
+
+  @Prop({ type: String, default: 'profile' })
+  object: string;
+
+  @Prop({ type: String, default: 'member' })
+  type: string;
+
+  @Prop({ type: String, default: 'active' })
+  status: string;
 }
 export type ProfileDocument = Profile & Document;
 export const ProfileSchema = SchemaFactory.createForClass(Profile);
