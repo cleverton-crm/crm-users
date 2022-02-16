547795d23379355a59093c1bee76fe9993dad2c0
icehuntmen <icehuntmen@gmail.com>
2022-02-16 15:51:34 +0300

    Mongo 4 replica

--- src/services/mongo.service.ts
+++ src/services/mongo.service.ts
@@ -14,13 +14,15 @@ export class MongoConfigService implements MongooseOptionsFactory {
     const host1 = process.env.MONGO_HOST1;
     const host2 = process.env.MONGO_HOST2;
     const host3 = process.env.MONGO_HOST3;
+    const host4 = process.env.MONGO_HOST4;
     const base = process.env.MONGO_DATABASE;
     const port1 = process.env.MONGO_PORT1;
     const port2 = process.env.MONGO_PORT2;
     const port3 = process.env.MONGO_PORT3;
+    const port4 = process.env.MONGO_PORT4;
 
     if (getBoolean(replica)) {
-      urlMongo = `mongodb://${user}:${password}@${host1}:${port1},${host2}:${port2},${host3}:${port3}/${base}?authSource=admin`;
+      urlMongo = `mongodb://${user}:${password}@${host1}:${port1},${host2}:${port2},${host3}:${port3},${host4}:${port4}/${base}?authSource=admin`;
     } else {
       urlMongo = `mongodb://${user}:${password}@${host1}:${port1}/${base}?authSource=admin`;
     }
